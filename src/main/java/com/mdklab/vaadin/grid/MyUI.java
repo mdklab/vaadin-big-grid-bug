a07ff778868c30e134b38f15829aae60fe3e6d41
mdk <d.k.maslennikov@gmail.com>
2017-07-28 10:13:24 +0300

    Bug of grid selection from row #441506

--- src/main/java/com/mdklab/vaadin/grid/MyUI.java
+++ src/main/java/com/mdklab/vaadin/grid/MyUI.java
@@ -1,16 +1,16 @@
 package com.mdklab.vaadin.grid;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.SerializableSupplier;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,20 +24,27 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        Grid<Person> grid = new Grid<>(Person.class);
+        grid.setSizeFull();
+        grid.setSelectionMode(Grid.SelectionMode.SINGLE);
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+        grid.getColumns().forEach(tColumn -> tColumn.setCaption(tColumn.getId()));
+        grid.setDataProvider((Grid.FetchItemsCallback<Person>) (sortOrder, offset, limit) -> getItems(offset, limit).stream()
+                , (SerializableSupplier<Integer>) this::getCount);
+
+        setContent(grid);
+    }
+
+    private Collection<Person> getItems(int offset, int limit) {
+        Collection<Person> res = new ArrayList<>(limit);
+        for (int i = offset; i < offset + limit; i++) {
+            res.add(new Person("Name " + i));
+        }
+        return res;
+    }
+
+    private Integer getCount() {
+        return 442_000;
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
---
+++ src/main/java/com/mdklab/vaadin/grid/Person.java
@@ -0,0 +1,22 @@
+package com.mdklab.vaadin.grid;
+
+/**
+ * <p>Title: </p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Description: <br>
+ *
+ * @author Dmitry K. Maslennikov (MDK)
+ * @version 1.0
+ * @since <pre>28.07.2017</pre>
+ */
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
